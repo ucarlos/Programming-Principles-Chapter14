1380c5eeacf3c353c127e86cc2fd4b591bfd897c
Ulysses Carlos <ulycarlos@gmail.com>
2020-10-15 17:08:58 -0400

    Finished Chapter14_06.cc and Chapter14_08.cc

---
+++ Chapter14_06.cc
@@ -0,0 +1,62 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Created by Ulysses Carlos on 10/15/2020 at 04:05 PM
+ *
+ * Chapter14_06.cc
+ * Define a Striped_circle using the technique from Striped_rectangle.
+ * -----------------------------------------------------------------------------
+ */
+
+#include "GUI/Simple_window.h"
+using namespace Graph_lib;
+using Simple_Window = Simple_window;
+
+struct Striped_Circle : Graph_lib::Circle {
+    Striped_Circle(Point center, int r) : Circle(center, r) { }
+    void draw_lines() const;
+
+};
+
+//------------------------------------------------------------------------------
+// distance() : Calculate the distance of two points using the distance
+// formula
+//------------------------------------------------------------------------------
+int distance(int x, int y, int xx, int yy){
+    return sqrt((x - xx) * (x - xx) + (y - yy) * (y - yy));
+}
+
+void Striped_Circle::draw_lines() const {
+    Circle::draw_lines(); // Call Circle's draw_lines
+    if (fill_color().visibility()) {
+
+	int x, y, xx, yy;
+
+	fl_color(fill_color().as_int()); // Set color:
+	// For each degree of a semi-circle:
+	double radian = M_PI / 180;
+	int width;
+	for (int i = 0; i < 180; i += 4){
+	    x = center().x + radius() * cos(radian * (90 + i));
+	    y = center().y + radius() * sin(radian * (90 + i));
+
+	    xx = center().x + radius() * cos(radian * (90 - i));
+	    yy = center().y + radius() * sin(radian * (90 - i));
+
+	    width = distance(x, y, xx, yy);
+	    fl_rect(x, y, width, 1);
+	}
+	
+
+    }
+
+
+}
+
+int main(void){
+    Simple_Window win{Point{0, 0}, 1280, 720, "Chapter14_06"};
+    Striped_Circle circ{Point{400, 400}, 200};
+    circ.set_fill_color(Color::blue);
+    win.attach(circ);
+    win.wait_for_button();
+
+}
---
+++ Chapter14_08.cc
@@ -0,0 +1,73 @@
+/*
+ * -----------------------------------------------------------------------------
+ * Created by Ulysses Carlos on 10/15/2020 at 04:38 PM
+ *
+ * Chapter14_08.cc
+ * Define a class Octagon to be a regular octagon. Write a test that exercises
+ * all of its functions (as defined by you or inherited from Shape).
+ * -----------------------------------------------------------------------------
+ */
+
+#include "GUI/Simple_window.h"
+#include <iostream>
+using namespace Graph_lib;
+using Simple_Window = Simple_window;
+
+struct Octagon : Shape {
+    Octagon(Point center, int rad) : radius{rad} { add(center); }
+    int get_radius() const { return radius; }
+    void set_radius(int r) { radius = r; }
+    Point get_center() const { return Point{point(0).x, point(0).y}; }
+    void draw_lines() const;
+    
+private:
+    int radius;
+};
+
+void Octagon::draw_lines() const {
+    double angle_radian = M_PI / 4;
+    Point center = get_center();
+
+    double x, y, xx, yy;
+    if (color().visibility()) {
+	fl_color(color().as_int());
+
+    }
+       
+    x = radius * cos(angle_radian);
+    y = radius * sin(angle_radian);
+
+    // Draw the first line
+    fl_line(center.x + radius, center.y,
+	    center.x + x, center.y + y);
+	
+    // For the next lines:
+    for (int i = 1; i < 7; i++){
+	xx = radius * cos((i + 1) * angle_radian);
+	yy = radius * sin((i + 1) * angle_radian);
+	
+	fl_line(center.x + x, center.y + y,
+		center.x + xx, center.y + yy);
+
+	// Now update x and y for the next angle:
+	x = xx; y = yy;
+    }
+	
+    // Now connect the first and last line:
+    fl_line(center.x + x, center.y + y,
+	    center.x + radius, center.y);    
+
+    
+}
+
+int main(){
+    Simple_Window win{Point{0, 0}, 1280, 720, "Chapter14_08"};
+    Octagon oct{Point{400, 400}, 200};
+    oct.set_radius(100);
+    std::cout << "Radius of Octagon: " << oct.get_radius() << std::endl;
+    oct.set_color(Color::red);
+    
+    
+    win.attach(oct);
+    win.wait_for_button();
+}
